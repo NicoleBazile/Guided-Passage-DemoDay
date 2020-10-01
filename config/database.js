49a4d74d1460440cc250897b048c7fa86be41093
Nicole B <63973798+NicoleBazile@users.noreply.github.com>
2020-09-30 22:39:23 -0400

    Delete database.js

--- config/database.js
+++
@@ -1,7 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : "mongodb+srv://GPAdmin:GPAdmin1103@cluster0.2wfgb.mongodb.net/patientDatabase?retryWrites=true&w=majority",
-
-    'dbName': 'patientDatabase'
-};
