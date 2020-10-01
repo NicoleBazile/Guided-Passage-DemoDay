f5fd0e17e302c7fdea4e9fc3e4d08e23539861b5
Nicole B <63973798+NicoleBazile@users.noreply.github.com>
2020-09-30 22:37:17 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : "mongodb+srv://NBwebdev:8n5LLTKbQGn87b8@cluster0.fe6hb.mongodb.net/RouletteGame?retryWrites=true&w=majority", // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'dbName': 'RouletteGame'
+    'url' : "mongodb+srv://GPAdmin:GPAdmin1103@cluster0.2wfgb.mongodb.net/patientDatabase?retryWrites=true&w=majority",
+
+    'dbName': 'patientDatabase'
 };
